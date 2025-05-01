347811da7712a84e2fa457d0a1f513ab3ef06155
ianspractice <bodybody.ian@gmail.com>
2025-05-01 10:29:17 -0600

    Create function to populate display

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 const buttons = Array.from(document.querySelectorAll("button"));
-
+const display = document.querySelector("#display");
 const divRows = Array.from(document.querySelectorAll(".row"));
 
+//change background colors
 function changeColor1(e) {
   e.target.style.backgroundColor = "purple";
 }
-
 function changeColor2(e) {
   e.target.style.backgroundColor = "pink";
 }
-
+//change buttons colors when clicked
 divRows.map((row) => {
   row.addEventListener("mousedown", changeColor1);
   row.addEventListener("mouseup", changeColor2);
+  row.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+  });
 });
 
-//create add function
+//operator functions
 function add(a, b) {
   return a + b;
 }
-
-// create subtract function
 function subtract(a, b) {
   return a - b;
 }
-
-//create multiply function
 function multiply(a, b) {
   return a * b;
 }
-
-//create divide function
 function divide(a, b) {
   return (a / b).toFixed(5);
 }
-
-//create "operate" function; takes operator and two numbers and calls one of the above functions
 function operate(a, op, b) {
   if (op === "+") {
     console.log(add(a, b));
@@ -52,7 +47,7 @@ function operate(a, op, b) {
   }
 }
 //create function to populate "display"
-
+function popDisplay(input) {
+  display.textContent = input;
+}
 //get user input; store as variables to use in functions; operate on them when user presses "="
-
-operate(2, "+", 3);
